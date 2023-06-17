a1c8720de773de9ee786338ceb3917ae10ade48d
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-17 18:15:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+let tel={
+	2:"abc",
+	3:"def",
+	4:"ghi",
+	5:"jkl",
+	6:"mno",
+	7:"pqrs",
+	8:"tuv",
+	9:"wxyz"
+}
 function letterCombinations(input_digit) {
   //Complete the function
+	let ans=[];
+	function findcombination(curidx,curcom) {
+		
+		if(curidx==input_digit.length){
+			ans.push(curcom);
+		}
+		let curchar=input_digit[curidx];
+		for(int i=0;i<tel.curchar.length;i++){
+			findcombination(curidx+1,curcom+tel[curchar][i]);
+		}
+	}
+
+	findcom(0,"");
+	return ans;
 }
 
 module.exports = letterCombinations;
